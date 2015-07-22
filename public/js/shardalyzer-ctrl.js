4b06b8353ed696738911fda2dd797234e7a3c821
Bernard Gorman <bernard.gorman@mongodb.com>
2015-07-22 10:32:21 +0100

    - use tc-chartjs charting library; example code working

--- public/js/shardalyzer-ctrl.js
+++ public/js/shardalyzer-ctrl.js
@@ -1,5 +1,5 @@
 
-var shardvis = angular.module('shard-vis', []).run
+var shardvis = angular.module('shard-vis', ["tc.chartjs"]).run
 (
 	function($rootScope)
 	{
@@ -100,26 +100,64 @@ shardvis.controller
 	}
 );
 
-/*
-angular.module("shard-vis", ["chart.js"]).controller("RadarCtrl", function ($scope, $http)
+shardvis.controller("showChart", function ($scope)
 {
-	$scope.labels =[];
-	$scope.data = [];
+	console.log('chart handler');
 
-	$http
-	({
-		method: 'GET',
-		url: 'http://localhost:3000/mongo/metadata/localhost/27017/bootcamp/twitter'
-	}).success
-	(
-		function (result)
-		{
-			for(entry in result['shard01'])
-			{
-				$scope.data.push(1);
-				$scope.labels.push(entry);
-			}
-		}
-	);
+	// Chart.js Data
+    $scope.data = [
+      {
+        value: 300,
+        color:'#F7464A',
+        highlight: '#FF5A5E',
+        label: 'Red'
+      },
+      {
+        value: 50,
+        color: '#46BFBD',
+        highlight: '#5AD3D1',
+        label: 'Green'
+      },
+      {
+        value: 100,
+        color: '#FDB45C',
+        highlight: '#FFC870',
+        label: 'Yellow'
+      }
+    ];
+
+    // Chart.js Options
+    $scope.options =  {
+
+      // Sets the chart to be responsive
+      responsive: true,
+
+      //Boolean - Whether we should show a stroke on each segment
+      segmentShowStroke : true,
+
+      //String - The colour of each segment stroke
+      segmentStrokeColor : '#fff',
+
+      //Number - The width of each segment stroke
+      segmentStrokeWidth : 2,
+
+      //Number - The percentage of the chart that we cut out of the middle
+      percentageInnerCutout : 50, // This is 0 for Pie charts
+
+      //Number - Amount of animation steps
+      animationSteps : 100,
+
+      //String - Animation easing effect
+      animationEasing : 'easeOutBounce',
+
+      //Boolean - Whether we animate the rotation of the Doughnut
+      animateRotate : true,
+
+      //Boolean - Whether we animate scaling the Doughnut from the centre
+      animateScale : false,
+
+      //String - A legend template
+      legendTemplate : '<ul class="tc-chart-js-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+
+    };
 });
-*/
