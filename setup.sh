c1519505066387d9d676efddbbd90233fb4260b1
Bernard Gorman <bernard.gorman@mongodb.com>
2015-08-05 23:50:12 +0100

    - add package manager checks to install nodejs/npm in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+if [[ ! -z $(which yum) ]]; then
+	sudo yum install epel-release
+	sudo yum install nodejs npm --enablerepo=epel
+elif [[ ! -z $(which apt-get) ]]; then
+	sudo apt-get install nodejs-legacy npm
+elif [[ ! -z $(which brew) ]]; then
+	brew install node
+elif [[ ! -z $(which port) ]]; then
+	port install nodejs npm
+elif [[ -z $(which npm) ]]; then
+	echo "Can't find node.js npm; exiting..."
+	exit 1;
+fi
+
 npm install
 ./node_modules/bower/bin/bower install
