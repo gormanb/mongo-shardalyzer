ee0a08812226efa5972f4b88496301c40736b465
Alex Bevilacqua <alex@alexbevi.com>
2018-12-31 12:12:38 -0500

    Upgrade Express and explicitely add middleware that is no longer bundled
    - serve-favicon
    - morgan
    - body-parser
    - method-override
    - errorhandler

--- mongo-shardalyzer.js
+++ mongo-shardalyzer.js
@@ -4,6 +4,11 @@
  */
 
 var express = require('express')
+  , favicon = require('serve-favicon')
+  , logger = require('morgan')
+  , bodyParser = require('body-parser')
+  , methodOverride = require('method-override')
+  , errorhandler = require('errorhandler')
   , routes = require('./routes')
   , user = require('./routes/user')
   , mongo = require('./routes/mongo')
@@ -16,18 +21,17 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorhandler());
 }
 
 //app.get('/', routes.index);
